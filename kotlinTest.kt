6368e0bdbef5852590b02c0fd10e2def330882dc
alxSeven <150055578+alxSeven@users.noreply.github.com>
2023-11-10 22:26:19 -0400

    Update kotlinTest.kt
    Adding the sum of all elements in the list

--- kotlinTest.kt
+++ kotlinTest.kt
@@ -1,14 +1,18 @@
 import kotlin.random.Random
 /*Un programa que genera una lista de 20
- *numeros enteros pseudo aleatorios
- *                                     */
+ *numeros enteros pseudo aleatorios,
+ *los imprime de forma tabular y calcula
+ *la suma de los elementos de la lista*/
 fun main() {
   var rndNumbers: List<Int> = List(20) { Random.nextInt() }
-  rndNumbers.forEachIndexed { index, value -> 
+  var sum = 0
+  rndNumbers.forEachIndexed { index, value ->
+    sum += value
     if(index % 5 == 0){
       println()
     }
     print("$value ")
   }
   println()
+  println("La sumatoria de los elementos de la lista es $sum")
 }
